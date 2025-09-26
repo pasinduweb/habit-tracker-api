38fe10a722232f561c438ef5d99dc55300b49b17
Pasindu Manaranjana <algopasindu@gmail.com>
2025-09-26 17:37:34 +0530

    env port console log changes

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { app } from './server.ts';
 import { env } from '../env.ts';
 
 app.listen(env.PORT, () => {
-    console.log(`Server running on http://localhost:${env.PORT}`);
+    console.log(`Server running on ${env.PORT}`);
 });
